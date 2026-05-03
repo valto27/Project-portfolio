6cf07fb7c8359c3480802806846b86c648d35f1a
Mattia Valtolina <mattia.valto@gmail.com>
2026-05-03 10:42:17 +0200

    feat: add smart-beta-backtest project to portfolio

---
+++ smart-beta-backtest/config.py
@@ -0,0 +1,13 @@
+start_date = "2005-01-01"
+end_date = "2024-12-31"
+
+tickers = ['AAPL', 'MSFT', 'JPM', 'JNJ', 'XOM', 'BAC', 'WMT', 'PG', 'CVX', 'HD',
+           'MRK', 'PFE', 'KO', 'PEP', 'ABBV', 'MCD', 'ABT', 'TMO', 'COST', 'DHR',
+           'ACN', 'NEE', 'LIN', 'TXN', 'UNH', 'HON', 'UPS', 'LOW', 'AMGN', 'SBUX',
+           'IBM', 'CAT', 'GS', 'BLK', 'AXP', 'MMM', 'BA', 'GE', 'F', 'GM',
+           'USB', 'WFC', 'C', 'MS', 'T', 'VZ', 'CMCSA', 'INTC', 'CSCO', 'QCOM',
+           'TGT', 'MDLZ', 'CL', 'EL', 'GIS', 'K', 'SYY', 'HSY', 'MKC', 'CPB',
+           'ETN', 'EMR', 'DOV', 'PH', 'ROK', 'ITW', 'AME', 'ADP', 'PAYX', 'AFL',
+           'ALL', 'TRV', 'CB', 'PGR', 'MET', 'PRU', 'APD', 'SHW', 'PPG', 'ECL',
+           'IFF', 'EMN', 'NUE', 'WY', 'PKG', 'BEN', 'TROW', 'STT', 'NTRS', 'FIS',
+           'BDX', 'BSX', 'SYK', 'ZBH', 'BAX', 'EW', 'RMD', 'HOLX', 'VAR', 'IDXX']
